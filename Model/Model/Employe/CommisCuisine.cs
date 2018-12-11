d4c9dc4e98466d3da30f7bb6e66f283768a49077
Nex45 <patricia.hu@viacesi.fr>
2018-12-11 11:01:53 +0100

    11/12/2018

---
+++ Model/Model/Employe/CommisCuisine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class CommisCuisine
+    {
+        public int id_employe {get; set;}
+        public string type {get; set;}
+        public Boolean disponible {get; set; }
+
+        public CommisCuisine(int id_employe, string type, Boolean disponible)
+        {
+            this.id_employe = id_employe;
+            this.type = type;
+            this.disponible = disponible;
+            Console.WriteLine("Nouveau " + type + " créé(e)");
+            Console.ReadKey();
+        }
+
+        public void ApporterCommande(Commande commande)
+        {
+            Console.WriteLine("Le commis apporte la commande : " + commande.client.nom);
+            Console.ReadKey();
+
+            //ComptoirCmdesPrep comptoir = new ComptoirCmdesPrep(commande, commande.serveur, commande.client);
+            //Ludo.ComptoirCmdesPrep(commande, Ludo, commande.client);
+        }
+    }
+}
---
+++ Model/Model/Salle/EtagereSalle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class EtagereSalle
+    {
+        // attributs
+        private int nbcarte;
+
+        private int nbeau;
+
+        private int nbpain;
+
+        public void decremCarte(int a)
+        {
+            this.nbcarte = this.nbcarte - a;
+        }
+
+        public void decremEau(int a)
+        {
+            this.nbeau = this.nbeau - a;
+        }
+
+        public void DecremPain(int a)
+        {
+            this.nbpain = this.nbpain - a;
+        }
+
+        public int GetCarte()
+        {
+            return this.nbcarte;
+        }
+
+        public int GetEau()
+        {
+            return this.nbeau;
+        }
+
+        public int GetPain()
+        {
+            return this.nbpain;
+        }
+
+        // les accesseurs en lecture
+        public void SetCarte(int nCarte)
+        {
+            this.nbcarte = nCarte;
+        }
+
+        public void SetEau(int nEau)
+        {
+            this.nbeau = nEau;
+        }
+
+        public void SetPain(int nPain)
+        {
+            this.nbpain = nPain;
+        }
+    }
+}
